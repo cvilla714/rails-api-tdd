cb0ee05c63f32c1611a65f516cec8108d28e54b0
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-27 02:14:19 -0400

    added simple test examples

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it('tests a number to be positive') do
+    expect(1).to be_positive
+  end
+
+  it('tests article object') do
+    # article = Article.create({title:'Sample title',content:"Sample content",slug:"sample-title"})
+    # article = FactoryBot.create(:article)
+    article = create(:article)
+    expect(article.title).to eq('Sample article')
+  end
 end
