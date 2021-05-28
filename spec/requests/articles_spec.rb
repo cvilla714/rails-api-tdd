f26b8dbd0bb925831d5c63cef4211511e6c72ca4
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-28 12:31:45 -0400

    added test for pagination and if the pagination includes the proper links

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -35,6 +35,18 @@ RSpec.describe ArticlesController do
       )
     end
 
-    it
+    it('paginates results') do
+      article1, article2, article3 = create_list(:article, 3)
+      get '/articles', params: { page: { number: 2, size: 1 } }
+      expect(json_data.length).to eq(1)
+      expect(json_data.first[:id]).to eq(article2.id)
+    end
+
+    it('paginates results') do
+      article1, article2, article3 = create_list(:article, 3)
+      get '/articles', params: { page: { number: 2, size: 1 } }
+      expect(json['links'].length).to eq(5)
+      expect(json['links'].keys).to contain_exactly('first', 'prev', 'next', 'last', 'self')
+    end
   end
 end
