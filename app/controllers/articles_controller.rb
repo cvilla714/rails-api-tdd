8ddc2f6eb295149c5558a1dfd8e6f01bbf625fa5
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-28 16:03:00 -0400

    added the show method and passed the pamars id, to render the  new aticlet serialized with json

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,11 @@ class ArticlesController < ApplicationController
     render_collection(paginated)
   end
 
+  def show
+  article = Article.find(params[:id])
+  render json:serializer.new(article)
+  end
+
   def serializer
     ArticleSerializer
   end
