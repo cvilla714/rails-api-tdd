55c3eebbe8401a4588fbc07d51c04eb6b75fb35a
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-28 15:28:48 -0400

    added the paginator, add params to the new paginator to fetch from the selecte page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,24 @@ class ArticlesController < ApplicationController
   def index
     # articles = Article.all
     articles = Article.recent
-    render json: serializer.new(articles), status: :ok
+    paginated = paginator.call(
+      articles,
+      params: pagination_params,
+      base_url: request.url
+    )
+    options = { meta: paginated.meta.to_h, links: paginated.links.to_h }
+    render json: serializer.new(paginated.items, options), status: :ok
   end
 
   def serializer
     ArticleSerializer
+  end
+
+  def paginator
+    JSOM::Pagination::Paginator.new
+  end
+
+  def pagination_params
+    params.permit![:page]
   end
 end
