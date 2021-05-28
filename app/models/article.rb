4533f4b3b0e81a9b76750ab5b1c29c744fa2c02b
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-28 02:09:51 -0400

    created the scope to list articles in descending order

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,7 @@ class Article < ApplicationRecord
   validates :content, presence: true
   validates :slug, presence: true, uniqueness: true
   #   validates :slug, uniqueness: true
+
+  # scope :recent, -> { order(created_at: :desc) }
+  scope :recent, -> { order(created_at: :desc) }
 end
