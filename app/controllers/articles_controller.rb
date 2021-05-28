7bce08569b21a5f77fdf12af664d642e5cda55a8
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-28 15:51:23 -0400

    including the module Paginable and usnig the render_collection method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,12 @@
 class ArticlesController < ApplicationController
+  include Paginable
+
   def index
-    # articles = Article.all
-    articles = Article.recent
-    paginated = paginator.call(
-      articles,
-      params: pagination_params,
-      base_url: request.url
-    )
-    options = { meta: paginated.meta.to_h, links: paginated.links.to_h }
-    render json: serializer.new(paginated.items, options), status: :ok
+    paginated = paginate(Article.recent)
+    render_collection(paginated)
   end
 
   def serializer
     ArticleSerializer
-  end
-
-  def paginator
-    JSOM::Pagination::Paginator.new
-  end
-
-  def pagination_params
-    params.permit![:page]
   end
 end
