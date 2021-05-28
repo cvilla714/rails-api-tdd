618b2c7dcdabb57397a81915a62911cc17eee532
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-27 20:23:04 -0400

    created the test to match the response json from the api

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -7,5 +7,24 @@ RSpec.describe ArticlesController do
       # expect(response.status).to eq(200)
       expect(response).to have_http_status(:ok)
     end
+
+    it('returns a proper JSON') do
+      article = create :article
+      get '/articles'
+      body = JSON.parse(response.body).deep_symbolize_keys
+      expect(body).to eq(
+        data: [
+          {
+            id: article.id.to_s,
+            type: 'article',
+            attributes: {
+              title: article.title,
+              content: article.content,
+              slug: article.slug
+            }
+          }
+        ]
+      )
+    end
   end
 end
