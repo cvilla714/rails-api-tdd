6eb288cd3c24af90a5c9d46746fb224556cf31dd
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-30 00:01:34 -0400

    added the token to the client using environmental variables

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -3,9 +3,31 @@ class UserAuthenticator
 
   attr_reader :user
 
-  def initialize(code); end
+  def initialize(code)
+    @code = code
+  end
 
   def perform
-    raise AuthenticationError
+    # client = Octokit::Client.new(
+    #   client_id: ENV['GITHUB_CLIENT_ID'],
+    #   client_secret: ENV['GITHUB_CLIENT_SECRET']
+    # )
+    github_token = Rails.application.credentials.github[:access_token]
+    client = Octokit::Client.new(github_token)
+
+    client = Octokit::Client.new
+    token = client.exchange_code_for_token(code)
+    if token.try(:error).present?
+      raise AuthenticationError
+    else
+      user_client = Octokit::Client.new(access_token: token)
+      user_data = user_client.user
+      slice(:login, :avatar_url, :url, :username)
+      User.create(user_data.merge(provider: 'github'))
+    end
   end
+
+  private
+
+  attr_reader :code
 end
