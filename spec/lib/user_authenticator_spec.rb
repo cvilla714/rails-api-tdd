937fda8c01456a00fc4d930504d2fd87988c7ee1
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-02 13:20:24 -0400

    new test to reuse the already existing user if it already exists

--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -48,6 +48,12 @@ describe UserAuthenticator do
         pp User.last
         expect(User.last.name).to eq('John Smith')
       end
+
+      it('should reuse already registered user') do
+        user = create :user, user_data
+        expect { subject }.not_to change { User.count }
+        expect(authenticator.user).to eq(user)
+      end
     end
   end
 end
