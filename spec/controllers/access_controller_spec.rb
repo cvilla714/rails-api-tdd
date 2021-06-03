6d0e85a88e99b017e011cc3cfc00286ef74a2342
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-03 12:59:48 -0400

    created the error body and passed in keys not strings

--- spec/controllers/access_controller_spec.rb
+++ spec/controllers/access_controller_spec.rb
@@ -2,10 +2,31 @@ require 'rails_helper'
 
 RSpec.describe AccessTokenController, type: :controller do
   describe '#create' do
-    context 'when invalid request' do
+    context 'when inalid request' do
+      let(:error) do
+        {
+          status: '401',
+          source: { pointer: '/code' },
+          title: 'Authentication code is invalid',
+          detail: 'You must provide a valid code in order to exchange it for token'
+        }
+
+        # {
+        #   'status' => '401',
+        #   'source' => { 'pointer' => '/code' },
+        #   'title' => 'Authentication code is invalid',
+        #   'detail' => 'You must provide a valid code in order to exchange it for token'
+        # }
+      end
+
       it('should return 401 status code') do
         post :create
         expect(response).to have_http_status(401)
+      end
+
+      it('should return proper error body') do
+        post :create
+        expect(json[:errors]).to include(error)
       end
     end
 
