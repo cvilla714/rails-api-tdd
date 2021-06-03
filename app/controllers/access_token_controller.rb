9b9c7b414d56eac429de7ddadfaae1ccbcedd11a
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-03 14:02:36 -0400

    added rescue_from using UserAuthenticator passing AuthenticationError with the private method authentication_error

--- app/controllers/access_token_controller.rb
+++ app/controllers/access_token_controller.rb
@@ -1,17 +1,29 @@
 class AccessTokenController < ApplicationController
+  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+
   def create
     authenticator = UserAuthenticator.new(params[:code])
-    begin authenticator.perform
-    rescue UserAuthenticator::AuthenticationError
-      error =
-        {
-          status: '401',
-          source: { pointer: '/code' },
-          title: 'Authentication code is invalid',
-          detail: 'You must provide a valid code in order to exchange it for token'
-        }
+    authenticator.perform
+  end
 
-      render json: { "errors": [error] }, status: 401
-    end
+  private
+
+  def authentication_error
+    error =
+      {
+        status: '401',
+        source: { pointer: '/code' },
+        title: 'Authentication code is invalid',
+        detail: 'You must provide a valid code in order to exchange it for token'
+      }
+
+    # {
+    #   'status' => '401',
+    #   'source' => { 'pointer' => '/code' },
+    #   'title' => 'Authentication code is invalid',
+    #   'detail' => 'You must provide a valid code in order to exchange it for token'
+    # }
+
+    render json: { "errors": [error] }, status: 401
   end
 end
