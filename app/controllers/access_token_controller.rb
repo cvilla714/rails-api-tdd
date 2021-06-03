18334f17d60f1e962f3b5c4a6441f30974f3a4e9
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-03 13:51:41 -0400

    added begin  and rescue and added the authenticator using the params code

--- app/controllers/access_token_controller.rb
+++ app/controllers/access_token_controller.rb
@@ -1,13 +1,17 @@
 class AccessTokenController < ApplicationController
   def create
-    error =
-      {
-        status: '401',
-        source: { pointer: '/code' },
-        title: 'Authentication code is invalid',
-        detail: 'You must provide a valid code in order to exchange it for token'
-      }
+    authenticator = UserAuthenticator.new(params[:code])
+    begin authenticator.perform
+    rescue UserAuthenticator::AuthenticationError
+      error =
+        {
+          status: '401',
+          source: { pointer: '/code' },
+          title: 'Authentication code is invalid',
+          detail: 'You must provide a valid code in order to exchange it for token'
+        }
 
-    render json: { "errors": [error] }, status: 401
+      render json: { "errors": [error] }, status: 401
+    end
   end
 end
