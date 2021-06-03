2e7bea07c9aa8dfb5c85d68b68236a2d058bcd0c
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-03 14:06:51 -0400

    extracted the code responsiable for handling the error

--- app/controllers/access_token_controller.rb
+++ app/controllers/access_token_controller.rb
@@ -1,29 +1,21 @@
 class AccessTokenController < ApplicationController
-  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+  # rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
 
   def create
     authenticator = UserAuthenticator.new(params[:code])
     authenticator.perform
   end
 
-  private
+  # private
 
-  def authentication_error
-    error =
-      {
-        status: '401',
-        source: { pointer: '/code' },
-        title: 'Authentication code is invalid',
-        detail: 'You must provide a valid code in order to exchange it for token'
-      }
-
-    # {
-    #   'status' => '401',
-    #   'source' => { 'pointer' => '/code' },
-    #   'title' => 'Authentication code is invalid',
-    #   'detail' => 'You must provide a valid code in order to exchange it for token'
-    # }
-
-    render json: { "errors": [error] }, status: 401
-  end
+  # def authentication_error
+  #   error =
+  #     {
+  #       status: '401',
+  #       source: { pointer: '/code' },
+  #       title: 'Authentication code is invalid',
+  #       detail: 'You must provide a valid code in order to exchange it for token'
+  #     }
+  #   render json: { "errors": [error] }, status: 401
+  # end
 end
