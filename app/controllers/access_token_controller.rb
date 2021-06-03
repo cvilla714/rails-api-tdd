2d4490dfb8820cbdae475d9324a7c9e171f534f3
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-03 13:00:18 -0400

    passed the error body and keys not strings

--- app/controllers/access_token_controller.rb
+++ app/controllers/access_token_controller.rb
@@ -1,5 +1,13 @@
 class AccessTokenController < ApplicationController
   def create
-    render json: {}, status: 401
+    error =
+      {
+        status: '401',
+        source: { pointer: '/code' },
+        title: 'Authentication code is invalid',
+        detail: 'You must provide a valid code in order to exchange it for token'
+      }
+
+    render json: { "errors": [error] }, status: 401
   end
 end
