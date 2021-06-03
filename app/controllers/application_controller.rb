3710673c6229ddf96e2550f4c38a9702925e69ff
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-03 14:07:31 -0400

    added the method for handling the authentication error to be available for all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,19 @@ class ApplicationController < ActionController::API
       'JsonapiErrorsHandler::Errors::NotFound'
   )
   rescue_from ::StandardError, with: ->(e) { handle_error(e) }
+
+  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+
+  private
+
+  def authentication_error
+    error =
+      {
+        status: '401',
+        source: { pointer: '/code' },
+        title: 'Authentication code is invalid',
+        detail: 'You must provide a valid code in order to exchange it for token'
+      }
+    render json: { "errors": [error] }, status: 401
+  end
 end
