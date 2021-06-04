6af8b6ad7f63fa4de5dd94308a2f7e4bf3ad89ad
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-03 23:59:59 -0400

    added share examples for unauthorized request

--- spec/controllers/access_controller_spec.rb
+++ spec/controllers/access_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AccessTokenController, type: :controller do
   describe '#create' do
-    context 'when inalid request' do
+    shared_examples_for 'unauthorized_requests' do
       let(:error) do
         {
           status: '401',
@@ -13,14 +13,30 @@ RSpec.describe AccessTokenController, type: :controller do
       end
 
       it('should return 401 status code') do
-        post :create
+        subject
         expect(response).to have_http_status(401)
       end
 
       it('should return proper error body') do
-        post :create
+        subject
         expect(json[:errors]).to include(error)
       end
+    end
+
+    context 'when no code provided' do
+      subject { post :create }
+      it_behaves_like 'unauthorized_requests'
+    end
+
+    context 'when invalid code provided' do
+      let(:github_error) { double('Sawyer::Resource', error: 'bad_verfication_code') }
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(:exchange_code_for_token).and_return(github_error)
+      end
+
+      subject { post :create, params: { code: 'invalid_code' } }
+      it_behaves_like 'unauthorized_requests'
     end
 
     context 'when success request' do
