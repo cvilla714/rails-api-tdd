70fc1d89d89b1a2d04dce335deed6d37cca32b33
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-04 01:59:28 -0400

    added the deleted route to match the controller access token and action destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/articles', to: 'articles#index '
   post 'login', to: 'access_token#create'
+  delete 'logout', to: 'access_token#destroy'
   resources :articles, only: %i[index show]
 end
