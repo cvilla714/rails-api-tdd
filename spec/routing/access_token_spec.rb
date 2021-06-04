658ff67eab49f335d18a404b365b7f5f472addb0
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-04 01:58:59 -0400

    testing the logout route to match the controller access token and action destroy

--- spec/routing/access_token_spec.rb
+++ spec/routing/access_token_spec.rb
@@ -4,4 +4,8 @@ describe 'access token routes' do
   it('should route to access_token create action') do
     expect(post('/login')).to route_to('access_token#create')
   end
+
+  it('should route to access_tokens destroy action') do
+    expect(delete('/logout')).to route_to('access_token#destroy')
+  end
 end
