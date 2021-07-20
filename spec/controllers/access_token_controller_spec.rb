3b6b0de7d5d9bfa893a3abd32dd6f3f4ce3b1a26
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-20 13:29:58 -0400

    changed variable to authentication_error and passed it to the include the json errors

--- spec/controllers/access_token_controller_spec.rb
+++ spec/controllers/access_token_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe AccessTokenController, type: :controller do
-  describe '#create' do
+  describe 'POST#create' do
     shared_examples_for 'unauthorized_requests' do
-      let(:error) do
+      let(:authentication_error) do
         {
           status: '401',
           source: { pointer: '/code' },
@@ -19,7 +19,7 @@ RSpec.describe AccessTokenController, type: :controller do
 
       it('should return proper error body') do
         subject
-        expect(json[:errors]).to include(error)
+        expect(json[:errors]).to include(authentication_error)
       end
     end
 
@@ -80,16 +80,27 @@ RSpec.describe AccessTokenController, type: :controller do
 
   describe 'Delete #destroy' do
     context 'when invalid request' do
+      let(:authorization_error) do
+        {
+          status: '403',
+          source: { pointer: '/headers/authorization' },
+          title: 'Not authorized',
+          detail: 'You have no right to access this resource'
+        }
+      end
+
+      subject { delete :destroy }
+
       it 'should return 403 status code' do
         subject
         expect(response).to have_http_status(:forbidden)
       end
-    end
 
-    # it('should return proper json') do
-    #   subject
-    #   expect(json[:error]).to eq(autorization_error)
-    # end
+      it('should return proper error json') do
+        subject
+        expect(json[:errors]).to include(authorization_error)
+      end
+    end
     context 'when valid request' do
     end
   end
