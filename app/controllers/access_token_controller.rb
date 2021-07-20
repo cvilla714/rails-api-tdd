46713e0def5aeb81017e445af7255860a9e603ea
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-20 16:18:54 -0400

    added the methods to extract the bearer token and assigned it to a variable to find the token in the database and assigned it to the current user, also applied safeguard when no token is set

--- app/controllers/access_token_controller.rb
+++ app/controllers/access_token_controller.rb
@@ -9,6 +9,19 @@ class AccessTokenController < ApplicationController
   end
 
   def destroy
-    raise AuthorizationError
+    # provided_token = request.authorization
+    # pp provided_token
+    provided_token = request.authorization&.gsub(/\ABearer\s/, '')
+    pp provided_token
+
+    access_token = AccessToken.find_by(token: provided_token)
+    pp access_token
+
+    current_user = access_token&.user
+    pp current_user
+
+    raise AuthorizationError unless current_user
+
+    current_user.access_token.destroy
   end
 end
