e1b37ec6a8ddf5e4c69a8853d812d01cc1afc804
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-29 12:23:14 -0400

    added new migration and user_id to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_204159) do
+ActiveRecord::Schema.define(version: 2021_07_29_152617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2021_06_02_204159) do
     t.string "slug"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +44,5 @@ ActiveRecord::Schema.define(version: 2021_06_02_204159) do
   end
 
   add_foreign_key "access_tokens", "users"
+  add_foreign_key "articles", "users"
 end
